02786d4722d161eb8abebd8b8d57311997ff1ddf
Frederik Durant <Frederik.Durant@pandora.be>
2017-05-02 21:10:57 +0000

    Neo4J container seems to do its job

---
+++ neo4jloader/populate_neo4j.py
@@ -0,0 +1,27 @@
+from neo4j.v1 import GraphDatabase, basic_auth
+from neo4j.exceptions import ServiceUnavailable, ClientError, AuthError, SecurityError
+from time import sleep
+import sys
+import os
+
+neo4j_uid, neo4j_pwd = os.environ['NEO4J_AUTH'].split('/')
+
+secondsToSleep = 10
+
+for i in range(5,0,-1):
+    try:
+        driver = GraphDatabase.driver("bolt://neo4j:7687", auth=basic_auth(neo4j_uid, neo4j_pwd))
+        session = driver.session()
+    except (ServiceUnavailable, ClientError, AuthError, SecurityError) as e:
+        if i == 1:
+            raise
+        sleep(secondsToSleep)
+        print('retry %d after catching %s; sleeping for %d seconds' % (i,str(e), secondsToSleep))
+    else:
+        break    
+        
+result = session.run('MERGE (g:genre {name:"pop"}) ON CREATE SET g.created = timestamp()')
+result = session.run("MATCH (a:genre) RETURN count(a) as numberOfGenres")
+
+for record in result:
+    print >> sys.stderr, record

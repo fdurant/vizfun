3c51268d6c8946c791af847653a747edeb099ef6
Frederik Durant <Frederik.Durant@pandora.be>
2017-05-21 21:15:25 +0000

    First attempt to authenticate and get some data through the Spotify Web API

--- node/app/server.js
+++ node/app/server.js
@@ -2,6 +2,16 @@
 
 const express = require('express');
 const morgan = require('morgan');
+const SpotifyWebApi = require('spotify-web-api-node');
+
+// Examples from https://github.com/thelinmichael/spotify-web-api-node
+
+// credentials are optional
+var spotifyApi = new SpotifyWebApi({
+    clientId : process.env.SPOTIFY_CLIENT_ID,
+    clientSecret : process.env.SPOTIFY_CLIENT_SECRET,
+    redirectUri : 'http://music.viz.fun/' + process.env.APP_REDIRECT_PATH
+});
 
 // Constants
 const PORT = 8000;
@@ -17,3 +27,20 @@ app.get('/', function (req, res) {
 
 app.listen(PORT);
 console.log('Running on http://localhost:' + PORT);
+
+// Get Elvis' albums
+spotifyApi.getArtistAlbums('43ZHCT0cAZBISjO8DG9PnE')
+    .then(function(data) {
+	console.log('Artist albums', data.body);
+    }, function(err) {
+	console.error(err);
+    });
+
+// Get the authenticated user
+spotifyApi.getMe()
+    .then(function(data) {
+	console.log('Some information about the authenticated user', data.body);
+    }, function(err) {
+	console.log('Something went wrong!', err);
+    });
+

63eb6ecb44db10368665fd8a5416409a08549ad4
Frederik Durant <Frederik.Durant@pandora.be>
2017-06-18 12:54:57 +0000

    Replaced sigma.js with cytoscape.js. Also made playlist icons smaller, and scrollable

--- node/app/public/app.js
+++ node/app/public/app.js
@@ -161,62 +161,44 @@ angular.module('musicVizFunApp', [])
 			    $scope.playlistArtistsByIDs[artistID] = response.data.artists[a];
 			}
 		    }
-		    
+		    $scope.cy.resize();
 		});
-	}
-
-	$scope.graph = {
-	    "nodes": [
-		{
-		    "id": "n0",
-		    "label": "A node",
-		    "x": 0,
-		    "y": 0,
-		    "size": 3
-		},
-		{
-		    "id": "n1",
-		    "label": "Another node",
-		    "x": 3,
-		    "y": 1,
-		    "size": 2
-		},
-		{
-		    "id": "n2",
-		    "label": "And a last one",
-		    "x": 1,
-		    "y": 3,
-		    "size": 1
-		}
-	    ],
-	    "edges": [
-		{
-		    "id": "e0",
-		    "source": "n0",
-		    "target": "n1"
-		},
-		{
-		    "id": "e1",
-		    "source": "n1",
-		    "target": "n2"
-		},
-		{
-		    "id": "e2",
-		    "source": "n2",
-		    "target": "n0"
-		}
-	    ]
 	};
 
-	// Inspired by https://jsfiddle.net/6voan7k9/
-	// Instantiate sigma:
-	$scope.s = new sigma({
-	    graph: $scope.graph,
-	    container: 'graph-container'
+	$scope.graph = [
+	    // nodes
+	    { data: { id: 'a' } },
+	    { data: { id: 'b' } },
+	    { data: { id: 'c' } },
+	    // edges
+	    {
+		data: {
+		    id: 'ab',
+		    source: 'a',
+		    target: 'b'
+		}
+	    }
+	];
+
+	$scope.graphStyle = [
+	    {
+		selector: 'node',
+		style: {
+		    shape: 'hexagon',
+		    'background-color': 'red',
+		    label: 'data(id)'
+		}
+	    }];
+	
+	$scope.cy = cytoscape({
+	    container:document.getElementById('cy'),
+	    elements: $scope.graph,
+	    layout: {name: 'circle'},
+	    style: $scope.graphStyle
 	});
 
 	$scope.refreshGraph = function() {
-	    $scope.s.refresh();
+	    $scope.cy.resize();
 	    $log.log("refreshed graph")
 	};
 	

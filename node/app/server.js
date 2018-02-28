d96dacf01273180ca6ad58aa302e9862b8ed68df
Frederik Durant <Frederik.Durant@pandora.be>
2018-02-28 16:58:50 +0000

    Next step in introduction of sessions

--- node/app/server.js
+++ node/app/server.js
@@ -25,6 +25,28 @@ const client_id = process.env.SPOTIFY_CLIENT_ID;
 const client_secret = process.env.SPOTIFY_CLIENT_SECRET;
 const redirect_uri = process.env.APP_REDIRECT_PATH;
 
+session.Session.prototype.login = function login(cb) {
+    var req = this.req;
+    this.regenerate(function (err) {
+	if(err) {
+	    cb(err);
+	    return;
+	}
+	req.session._loggedInAt = Date.now();
+	req.session._ip = req.ip;
+	req.session._ua = req.headers['user-agent'];
+    });
+}
+
+session.Session.prototype.isLoggedIn = function isLoggedIn() {
+    return !!this._loggedInAt;
+}
+
+session.Session.prototype.isFresh = function isFresh() {
+    // Return true if logged in less than 20 minutes ago
+    return (this._loggedInAt && (Date.now() - this._loggedInAt) < (1000 * 60 * 20))
+}
+
 // Constants
 const PORT = 8000;
 
@@ -43,11 +65,20 @@ app.use(session({
 	host:redis_host,
 	port:redis_port,
 	db: 2,
-	pass: redis_pwd
+	pass: redis_pwd,
+	ttl: (1 * 60), // Time-to-Live in seconds
+	logErrors: true
     }),
+    name: 'id',
     secret:redis_secret,
     resave:false,
-    saveUninitialized:true
+    saveUninitialized:false,
+    cookie: {
+	secure: true, // Set the cookie only to be served with HTTPS
+	path: '/',
+	httpOnly: true, // Mitigate XXS (cross-site scripts)
+	maxAge: null
+    }
 }));
 
 app.get('/', function (req, res) {
@@ -61,8 +92,13 @@ app.get('/', function (req, res) {
 // Copied from https://github.com/spotify/web-api-auth-examples/blob/master/authorization_code/app.js
 app.get('/login', function(req, res) {
 
+    req.session.login();
+    
     var state = generateRandomString(16);
     res.cookie(stateKey, state);
+    console.log('state = ', state);
+    req.session._spotify_auth_state = state;
+    console.log('req.session._spotify_auth_state = ', req.session._spotify_auth_state);
 
     // your application requests authorization
     var scope = 'user-read-private user-read-email playlist-read-private';
@@ -77,6 +113,13 @@ app.get('/login', function(req, res) {
 		 }));
 });
 
+app.get('/logout', function(req, res) {
+
+    req.session.destroy(); // Delete session
+    
+});
+
+
 app.get('/callback', function(req, res) {
 
     // your application requests refresh and access tokens
@@ -84,7 +127,13 @@ app.get('/callback', function(req, res) {
 
     var code = req.query.code || null;
     var state = req.query.state || null;
+    console.log('code = ', code);
+    console.log('state = ', state);
+    console.log('req.session = ', req.session);
+    console.log('req.session._spotify_auth_state = ', req.session._spotify_auth_state);
+//    var storedState = req.session._spotify_auth_state ? req.session._spotify_auth_state : null;
     var storedState = req.cookies ? req.cookies[stateKey] : null;
+    console.log('storedState = ', storedState);
 
     if (state === null || state !== storedState) {
 	res.redirect('/#' +
@@ -112,9 +161,9 @@ app.get('/callback', function(req, res) {
 		var access_token = body.access_token;
 		var refresh_token = body.refresh_token;
 
-		// STORE AS COOKIE FOR LATER USE
-		res.cookie('spotifyAccessTokenForMusicVizFun', access_token, { maxAge: 86400, httpOnly: true });
-		res.cookie('spotifyRefreshTokenForMusicVizFun', refresh_token, { maxAge: 86400, httpOnly: true });
+		// STORE AS SESSION VARIABLE FOR LATER USE
+		req.session._spotifyAccessTokenForMusicVizFun = access_token;
+		req.session._spotifyRefreshTokenForMusicVizFun = refresh_token;
 		spotifyApi.setAccessToken(access_token);
 		spotifyApi.setRefreshToken(refresh_token);
 
@@ -146,12 +195,12 @@ app.get('/me', function(req, res) {
 	.then(function(data) {
 	    console.log('Some information about the authenticated user', data.body);
 	    console.log('data.body.id = ', data.body.id);
-	    res.cookie('spotifyCurrentUserId', data.body.id, { maxAge: 86400, httpOnly: true });
+	    req.session._spotifyCurrentUserId = data.body.id;
 	    //	    res.redirect('/playlists'); 
 //	    res.redirect('/#'); 
 	    res.send(data);
 	}, function(err) {
-	    console.log('Something went wrong!', err);
+	    console.log('User not (yet/anymore) authenticated with Spotify!', err);
 	    res.send({'err': err});
 //	    res.redirect('/#');
 	});
@@ -185,7 +234,7 @@ app.get('/refresh_token', function(req, res) {
 
 app.get('/playlists', function(req, res) {
 
-    var currentUserId = req.cookies.spotifyCurrentUserId;
+    var currentUserId = req.session._spotifyCurrentUserId;
     
     var playlists = spotifyApi.getUserPlaylists(currentUserId)
 	.then(function(data) {
